0b8d1881c0ee2941fadecef70a775325e861fd9e
fehoon.Hyeon <clearch21@gmail.com>
2025-03-20 21:54:29 +0900

    Initial commit: Setup with spring boot

---
+++ src/main/java/com/fehoon/sns/post/SnsPostServiceApplication.java
@@ -0,0 +1,13 @@
+package com.fehoon.sns.post;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnsPostServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnsPostServiceApplication.class, args);
+	}
+
+}
